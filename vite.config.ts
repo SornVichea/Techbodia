c45a047442597bfaeb51cb0537a1728c24a49841
SornVichea <vicheasorn2017@gmail.com>
2023-11-23 13:19:13 +0700

    Updates

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-// A1
-// vite.config.js
-export default defineConfig({
-  base: 'https://github.com/SornVichea/Techbodia.git', // Adjust this based on your GitHub repository name
-  plugins: [vue()],
-});
